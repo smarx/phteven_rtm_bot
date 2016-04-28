1139712a85f65bbc4dda2e061c12322898ddf7a6
Ryan M <ryan@ryanmo.co>
2016-04-27 19:23:52 -0700

    thats what she said

--- phteven_marquarst.py
+++ phteven_marquarst.py
@@ -137,6 +137,11 @@ def process_message(data):
     ])
 
     rules.add_many_inclusive([
+        # That's what she said
+        (
+            lambda data: "harder" in data.text,
+            lambda: "That's what she said"
+        ),
         # Say what smarx said...but a while ago
         (
             test_smarx,
